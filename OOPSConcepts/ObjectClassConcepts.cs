49b38ab3afd4bbe2bf440b23f5dcbe029a660906
Rak65 <rakeshp6505@gmail.com>
2023-09-25 21:38:55 +0530

    [Rakesh] Add -: Object-Class-Concepts

---
+++ OOPSConcepts/ObjectClassConcepts.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace OOPSConcepts
+{
+    public class ObjectClassConcepts
+    {
+        public string ClassName { get; set; }
+        public string ObjectName { get; set; }
+
+        public ObjectClassConcepts(string className, string objectName)
+        {
+            ClassName = className;
+            ObjectName = objectName;
+        }
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine($"ClassName: {ClassName}, ObjectName: {ObjectName}");
+        }
+    }
+}
